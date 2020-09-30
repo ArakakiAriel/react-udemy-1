e8e08ba26a970963cc4019091060bd3772587568
ArielArakaki <arakaki.arielkenji@gmail.com>
2020-09-30 18:44:12 +0900

    added create-react-app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import {Typography} from '@material-ui/core'
+
+function App() {
+  return (
+    <div className="app">
+      <Typography className="primary" color="red">
+        <h1>HOLA MUNDO</h1>
+      </Typography>
+    </div>
+  );
+}
+
+export default App;
